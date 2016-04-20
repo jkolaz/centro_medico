4d2ed23506795e1b01e361e1f416e4f7723ed6e0
Julio Salsavilca <j.salsavilca@gmail.com>
2016-04-20 18:05:56 -0500

    nose

--- admin/models/admin/administrador_model.php
+++ admin/models/admin/administrador_model.php
@@ -64,8 +64,48 @@ class Administrador_model extends CI_Model{
     
     public function generateClaveReset($id){
         $update = array();
-        $update['adm_clave_reset'] = uniqid("CM_");
+        $update['adm_clave_reset'] = uniqid(PREFIJO);
         $update['adm_status'] = 2;
-        $this->db->update(self::$_table, $update);
+        
+        $where = array();
+        $where['adm_id'] = $id;
+        $this->db->where($where)->update(self::$_table, $update);
     }
+    
+    public function getModulesToacces($tipo, $id_query = "", $mode = ""){
+        $where_add = "";
+        if ($id_query != "") {
+            switch ($mode) {
+                case "+": //only
+                    $where_add = " and cm_secciones.sec_id = '" . $id_query . "' ";
+                    break;
+                case "-":
+                    if (!is_array($id_query)) {
+                        $id_query[] = $id_query;
+                    }
+                    foreach ($id_query as $idss) {
+                        $where_add .= " and cm_secciones.sec_id != '" . $idss . "' ";
+                    }
+
+                    break;
+            }
+        }
+        $sql = "select cm_secciones.*
+                from cm_tipo_administrador_pagina, cm_secciones
+                where
+                        cm_tipo_administrador_pagina.tap_id = '" . $tipo . "' and
+                        cm_tipo_administrador_pagina.ra_pp_ra_s_id = cm_secciones.sec_id and
+                        cm_secciones.s_status = 1
+                        " . $where_add . "
+                group by cm_tipo_administrador_pagina.ra_pp_ra_s_id
+                order by cm_secciones.sec_nombre
+                ";
+        $query = $this->db->query($sql);
+        if($query->num_rows > 0){
+            return $query->result();
+        }else{
+            return null;
+        }
+    }
+    
 }
